7a6043f17c61f47574285c1d4433fa79a70b28bf
Seafoamm <brancel8@gmail.com>
2026-03-08 19:11:20 -0700

    remove tailwind and ensure JetBrains mono font

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-# Ensures all background processes (Tailwind) die when you Ctrl+C
 trap "kill 0" EXIT
 
-ROOT=$(pwd)
-
-npx @tailwindcss/cli -i "$ROOT/input.css" -o "$ROOT/assets/output.css" --watch=always < /dev/null &
-
 echo "🦀 Cargo Watcher Starting..."
-# Requires 'cargo install cargo-watch'
-# This restarts your Axum server whenever .rs or .html files change
-cargo watch -w src -w assets -x run
+cargo watch -w src -w assets/styles.css -x run
--- src/templates.rs
+++ src/templates.rs
@@ -1,4 +1,4 @@
-use maud::{Markup, html};
+use maud::{DOCTYPE, Markup, html};
 
 fn tmpl_term_prompt(content: &str) -> Markup {
     html! {
@@ -12,60 +12,54 @@ pub(crate) async fn tmpl_example_term_prompt() -> Markup {
 
 pub(crate) async fn tmpl_index() -> Markup {
     html! {
+        (DOCTYPE)
         html {
             head {
                 meta charset="UTF-8";
-                meta name="viewport" content="width=device-width, initial-scale=1.0";
-                link href="/output.css" rel="stylesheet";
-                link type="image/x-icon" href="./favicon.png" rel="icon";
-                script src="https://unpkg.com/hyperscript.org@0.9.14" {}
+                link href="/styles.css" rel="stylesheet";
+                link
+                    rel="icon"
+                    type="image/png"
+                    href="./favicon.png"
+                    script
+                    src="https://unpkg.com/hyperscript.org@0.9.14" {}
+                title { "Brandon Lu - Resume" }
             }
-            header."bg-surface border border-surface shadow-sm mb-4 font-primary text-primary flex-row flex justify-between" {
-                div."ml-2 flex-1 text-left py-0" {
-                    p."text-xs" {
-                        "Placeholder for icons/CTA"
-                    }
-                }
-                div."flex-1 flex-col py-2 text-center" {
-                    a href="." {
-                        h1."py-4 text-primary text-4xl flex-1 justify-evenly bg-surface font-primary text-center" {
-                            "Lu::Brandon"
+            body."clear-spacing" {
+                header."term-window-title-chrome" {
+                    div."term-window-title-left" {
+                        ul."term-window-title-socials clear-spacing" {
+                            a href="https://www.youtube.com/@DoubleColon11" {
+                                img src="./yt_icon.png";
+                            }
+                            a href="https://linktr.ee/doublecolon11" {
+                                img src="./linktree_icon.webp";
+                            }
                         }
                     }
-                    p {
-                        "( Brandon Lu )"
+                    div."term-window-title-center" {
+                        a href="." {
+                            h1 { "Lu::Brandon" }
+                        }
+                        p { "( Brandon Lu )" }
                     }
-                }
-                p."text-xs mr-2 flex-1 text-right py-0" {
-                    "Terminal UI inspired resume/CV/portfolio/blog"
-                }
-            }
-            nav."border border-surface shadow-sm flex-col flex gap-2 py-4 bg-surface" {
-                ul."justify-evenly flex gap-2 width-full" {
-                    a."text-accent" href="." {
-                        "About"
-                    }
-                    a."text-accent" href="/system_design" {
-                        "System Design"
-                    }
-                    a."text-accent" href="/algorithms" {
-                        "Algorithms"
+                    p."term-window-title-right clear-spacing" {
+                        "Terminal UI inspired resume/CV/portfolio/blog"
                     }
                 }
                 hr;
-                ul."[&_img]:border [&_img]:border-surface [&_img]:p-2 [&_img]:size-12 flex flex-row gap-2 justify-center" {
-                    a href="https://www.youtube.com/@DoubleColon11" {
-                        img src="./yt_icon.png";
-                    }
-                    a href="https://linktr.ee/doublecolon11" {
-                        img src="./linktree_icon.webp";
+                nav."term-window-nav" {
+                    ul."term-window-nav-tab-bar" {
+                        a href="." { "About" }
+                        a href="/system_design" { "System Design" }
+                        a href="/algorithms" { "Algorithms" }
                     }
                 }
-            }
-            body."bg-main text-frost" {
-                "1. Intro
-  2. Resume
-  3. Projects Showcase"
+                hr;
+
+                "1. Intro\n
+                2. Resume\n
+                3. Projects Showcase"
             }
         }
     }
